533e26da51b4d8fa663c5b90ad5e9877c412c8f7
IDOKO EMMANUEL <eemmanuel.idoko@gmail.com>
2023-10-09 19:27:51 +0100

    File uploads
    Upload of application deployment file, model file and chrome driver

---
+++ prediction_app.py
@@ -0,0 +1,23 @@
+import uvicorn
+from fastapi import FastAPI
+import joblib, os
+
+app = FastAPI()
+
+#pkl
+phish_model = open('phishing.pkl', 'rb')
+phish_model_ls = joblib.load(phish_model)
+
+# ML Aspect
+@app.get('/predict/{feature}')
+async def predict(features):
+    x_predict = []
+    x_predict.append(str(features))
+    y_predict = phish_model_ls.predict(x_predict)
+    if y_predict == 'bad':
+        result = "This is a Phishing Site"
+    else:
+        result = "This is not a Phishing Site"
+    return (features, result)
+if __name__ == '__main__':
+    uvicorn.run(app, host="127.0.0.1", port=8000)
